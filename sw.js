0b86312fd3d0808a4bbeb9e94c00a9334c9a513b
Barshon13 <mohammadborshonhossain6@gmail.com>
2026-09-08 16:27:56 +0000

    feat(monetization): add Monetag multi-tag script and update sw.js zone

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11743647
+    "domain": "5gvci.com",
+    "zoneId": 11754854
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
